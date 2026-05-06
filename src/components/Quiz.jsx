4f7c9f8716117c4c9a38854731b2c7f3225ed568
dai ca <vochieu1902@gmail.com>
2026-05-06 11:30:58 +0700

    4.Thêm màn hình kết thúc và xáo trộn câu trả lời Quiz App

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,41 @@
 import { useState } from "react";
+import QuizCompleteImg from "../assets/quiz-complete.png";
+
 import QUESTIONS from "../questions.js";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+
+  //Kiểm tra kết thúc câu hỏi chưa
+  const quizComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
+
+  if (quizComplete) {
+    return (
+      <div id="summary">
+        <img src={QuizCompleteImg} />
+        <h2>Quiz completed</h2>
+      </div>
+    );
+  }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i >= 1; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  //tạo bản sao mảng gốc 0 để xáo trộn (0 làm thay đổi mảng gốc) -
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffleArray(shuffledAnswers);
+
   return (
     <>
       <div id="quiz">
@@ -16,7 +43,8 @@ export default function Quiz() {
           <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 
           <ul id="answers">
-            {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+            {/* {QUESTIONS[activeQuestionIndex].answers.map((answer) => { */}
+            {shuffledAnswers.map((answer) => {
               return (
                 <li
                   key={answer}
