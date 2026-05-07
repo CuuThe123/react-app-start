5b386895392facbb1a5091b399f1eb2bebe36f1b
dai ca <vochieu1902@gmail.com>
2026-05-07 15:43:11 +0700

    14. Vì Sao Progress Trong React Chạy Sai Thời Gian

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,6 +9,14 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
     isCorrect: null,
   });
 
+  let timer = 10000;
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
   function handleSelectedAnswer(answer) {
     setAnswer({
       selectedAnswer: answer,
@@ -36,8 +44,10 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
     <>
       <div id="question">
         <QuestionTimer
-          timeout={10000}
-          onTimeout={onSkipAnswer}
+          key={timer}
+          timeout={timer}
+          onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+          mode={answerState}
         />
         <h2>{QUESTIONS[index].text}</h2>
         <Answers
--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
   useEffect(() => {
     console.log("Timeout được gọi");
@@ -26,6 +26,7 @@ export default function QuestionTimer({ timeout, onTimeout }) {
       <progress
         max={timeout}
         value={remainingTime}
+        className={mode}
       ></progress>
     </>
   );
